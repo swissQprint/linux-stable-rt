979e0c0bdb560f829f031dbd616451e7fec9c24e
Thomas Gleixner <tglx@linutronix.de>
2023-09-16 21:26:03 +0206

    printk: nbcon: Add buffer management
    In case of hostile takeovers it must be ensured that the previous
    owner cannot scribble over the output buffer of the emergency/panic
    context. This is achieved by:
    
     - Adding a global output buffer instance for the panic context.
       This is the only situation where hostile takeovers can occur and
       there is always at most 1 panic context.
    
     - Allocating an output buffer per non-boot console upon console
       registration. This buffer is used by the console owner when not
       in panic context. (For boot consoles, the existing shared global
       legacy output buffer is used instead. Boot console printing will
       be synchronized with legacy console printing.)
    
     - Choosing the appropriate buffer is handled in the acquire/release
       functions.
    
    Co-developed-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Thomas Gleixner (Intel) <tglx@linutronix.de>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20230916192007.608398-5-john.ogness@linutronix.de
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/console.h
+++ include/linux/console.h
@@ -236,6 +236,7 @@ enum nbcon_prio {
 };
 
 struct console;
+struct printk_buffers;
 
 /**
  * struct nbcon_context - Context for console acquire/release
@@ -246,6 +247,7 @@ struct console;
  *				be used only with NBCON_PRIO_PANIC @prio. It
  *				might cause a system freeze when the console
  *				is used later.
+ * @pbufs:			Pointer to the text buffer for this context
  */
 struct nbcon_context {
 	/* members set by caller */
@@ -253,6 +255,9 @@ struct nbcon_context {
 	unsigned int		spinwait_max_us;
 	enum nbcon_prio		prio;
 	unsigned int		allow_unsafe_takeover	: 1;
+
+	/* members set by acquire */
+	struct printk_buffers	*pbufs;
 };
 
 /**
@@ -276,6 +281,7 @@ struct nbcon_context {
  * @node:		hlist node for the console list
  *
  * @nbcon_state:	State for nbcon consoles
+ * @pbufs:		Pointer to nbcon private buffer
  */
 struct console {
 	char			name[16];
@@ -298,6 +304,7 @@ struct console {
 
 	/* nbcon console specific members */
 	atomic_t		__private nbcon_state;
+	struct printk_buffers	*pbufs;
 };
 
 #ifdef CONFIG_LOCKDEP
